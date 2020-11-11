cf9de383fc0cc578925e25c0bcb6599e95ce97f2
daniel156161 <d.dolezal97@protonmail.com>
2020-11-11 20:44:48 +0100

    remove Ping of phone and check Player ID into the Spotify API and switch to Computer if it is unlocked

---
+++ logic.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+#Programmer: Daniel Dolezal
+#Write Date: 21.09.2020, 11.11.2020
+#Use: Start Spotify when Phone is home and Transfer Playback to PC
+
+SERVICE="spotify"
+keyword=is_playing
+Computer=$HOSTNAME
+configfile=/home/$USER/.config/Spotify_tokens.sh
+
+if [ -z $1 ]; then
+#check if Computer is unlocked or locked
+dbus-monitor --session "type='signal',interface='org.gnome.ScreenSaver'" |
+  while read x; do
+    case "$x" in 
+      *"boolean true"*) 
+      echo SCREEN_LOCKED
+      touch /tmp/stopSpotify.txt
+      ;;
+      *"boolean false"*)
+      echo SCREEN_UNLOCKED
+      rm -rf /tmp/stopSpotify.txt
+      ;;  
+    esac
+  done
+else
+ #Make config if not exist
+ if [ ! -f $configfile ]; then
+  path=$(echo $0 | rev | cut -d "/" -f "2-" | rev)
+  sh $path/make_config.sh
+ fi
+ ##Spotify Variables
+ . $configfile
+ REDIRECT_URI="http://localhost/"
+ #Exist refresh_token?
+ if [ ! -z $refresh_token ]; then
+  #Get Access Token
+  access_token=$(curl -s -d client_id=$CLIENT_ID -d client_secret=$CLIENT_SECRET -d grant_type=refresh_token -d refresh_token=$refresh_token https://accounts.spotify.com/api/token | python3 -c "import sys, json; print(json.load(sys.stdin)['access_token'])")
+  #Loop Checking if Spotify is running or not
+  while true; do
+   clear
+   while [ -f /tmp/stopSpotify.txt ]; do
+    echo "Wait for unlock of Computer"
+    sleep 10
+   done
+   if pgrep -x "$SERVICE" >/dev/null; then
+    echo "$SERVICE is running"
+    playerid=$(curl -s -X "GET" "https://api.spotify.com/v1/me/player" -H "Accept application/json" -H "Authorization: Bearer $access_token" | grep -A 1 "id" | tr "," "\n" | head -n1 | cut -d ":" -f "2")
+    #Get Computer ID from Spotify
+	if [ -z $ComputerID ]; then
+	 ComputerID=$(curl -sS -X "GET" "https://api.spotify.com/v1/me/player/devices" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $access_token" | python -mjson.tool | grep 'id\|name' | sed 's/"id": //g' | sed 's/"name": //g' | tr "," "\n" | grep -B 2 "$Computer" | head -n1 )
+	fi
+    if [ $ComputerID != $playerid ]; then
+     #Switch Playback to Computer if it play somewhere else
+     curl -X "PUT" "https://api.spotify.com/v1/me/player" --data "{\"device_ids\":[$ComputerID]}" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $access_token"
+    fi
+   else
+    #get if Spotify is Playing on any Device
+    api=$(curl -s -X "GET" "https://api.spotify.com/v1/me/player" -H "Authorization: Bearer $access_token" | python3 -c "import sys, json; print(json.load(sys.stdin)['$keyword'])" 2> /dev/null)
+    #If Yes run Code
+    if [ "$api" == "True" ]; then
+     playerid=$(curl -s -X "GET" "https://api.spotify.com/v1/me/player" -H "Accept application/json" -H "Authorization: Bearer $access_token" | grep -A 1 "id" | tr "," "\n" | head -n1 | cut -d ":" -f "2")
+     echo "Spotify is playing on other Device start $SERVICE"
+     $SERVICE & 2> /dev/null
+     sleep 5
+     #Get Computer ID from Spotify
+     ComputerID=$(curl -sS -X "GET" "https://api.spotify.com/v1/me/player/devices" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $access_token" | python -mjson.tool | grep 'id\|name' | sed 's/"id": //g' | sed 's/"name": //g' | tr "," "\n" | grep -B 2 "$Computer" | head -n1 )
+    else
+     echo "Spotify is not playing"
+     checktoken=$(curl -s -X "GET" "https://api.spotify.com/v1/me/player" -H "Authorization: Bearer $access_token" ) #2> /dev/null)
+     checktoken=$(echo $checktoken | cut -d " " -f "5-5" | sed -e 's/,//g')
+     #Make new Access Token is old Token is expired
+     if [ ! -z $checktoken ] && [ $checktoken == "401" ]; then
+      echo "Getting new access_token"
+      access_token=$(curl -s -d client_id=$CLIENT_ID -d client_secret=$CLIENT_SECRET -d grant_type=refresh_token -d refresh_token=$refresh_token https://accounts.spotify.com/api/token | python3 -c "import sys, json; print(json.load(sys.stdin)['access_token'])")
+     fi
+    fi
+   fi
+   sleep 10
+  done
+ else
+  echo "Please get refresh_token and add it into /home/$USER/.config/Spotify_tokens.sh" 
+ fi
+fi
--- run.sh
+++ run.sh
@@ -1,58 +1,9 @@
 #!/bin/bash
 
 #Programmer: Daniel Dolezal
-#Write Date: 21.09.2020
-#Use: Start Spotify when Phone is home and Transfer Playback to PC
+#Write Date: 11.11.2020
+#Use: Run all probably
 
-SERVICE="spotify"
-keyword=is_playing
-Computer=$HOSTNAME
-configfile=/home/$USER/.config/Spotify_tokens.sh
-
-#Make config if not exist
-if [ ! -f $configfile ]; then
- path=$(echo $0 | rev | cut -d "/" -f "2-" | rev)
- sh $path/make_config.sh
-fi
-##Spotify Variables
-. $configfile
-REDIRECT_URI="http://localhost/"
-#Exist refresh_token?
-if [ ! -z $refresh_token ]; then
- #Get Access Token
- access_token=$(curl -s -d client_id=$CLIENT_ID -d client_secret=$CLIENT_SECRET -d grant_type=refresh_token -d refresh_token=$refresh_token https://accounts.spotify.com/api/token | python3 -c "import sys, json; print(json.load(sys.stdin)['access_token'])")
- #Loop Checking if Spotify is running or not
- while true; do
-  clear
-  if pgrep -x "$SERVICE" >/dev/null; then
-   echo "$SERVICE is running"
-  else
-   #Check if Phone is Pingable
-   while ! ping -c1 $ping_url 2>/dev/null 1>&2; do sleep 10; done
-   #get if Spotify is Playing on any Device
-   api=$(curl -s -X "GET" "https://api.spotify.com/v1/me/player" -H "Authorization: Bearer $access_token" | python3 -c "import sys, json; print(json.load(sys.stdin)['$keyword'])" 2> /dev/null)
-   #If Yes run Code
-   if [ "$api" == "True" ]; then
-    echo "Spotify is playing on other Device start $SERVICE"
-    $SERVICE & 2> /dev/null
-    sleep 15
-    #Get Computer ID from Spotify
-    ComputerID=$(curl -sS -X "GET" "https://api.spotify.com/v1/me/player/devices" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $access_token" | python -mjson.tool | grep 'id\|name' | sed 's/"id": //g' | sed 's/"name": //g' | tr "," "\n" | grep -B 2 "$Computer" | head -n1 )
-    #Switch Playback to Computer
-    curl -X "PUT" "https://api.spotify.com/v1/me/player" --data "{\"device_ids\":[$ComputerID]}" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $access_token"
-   else
-    echo "Spotify is not playing"
-    checktoken=$(curl -s -X "GET" "https://api.spotify.com/v1/me/player" -H "Authorization: Bearer $access_token" ) #2> /dev/null)
-    checktoken=$(echo $checktoken | cut -d " " -f "5-5" | sed -e 's/,//g')
-    #Make new Access Token is old Token is expired
-    if [ $checktoken == "401" ]; then
-     echo "Getting new access_token"
-     access_token=$(curl -s -d client_id=$CLIENT_ID -d client_secret=$CLIENT_SECRET -d grant_type=refresh_token -d refresh_token=$refresh_token https://accounts.spotify.com/api/token | python3 -c "import sys, json; print(json.load(sys.stdin)['access_token'])")
-    fi
-   fi
-  fi
-  sleep 10
- done
-else
- echo "Please get refresh_token and add it into /home/$USER/.config/Spotify_tokens.sh" 
-fi
+path=$(echo $0 | rev | cut -d "/" -f "2-" | rev)
+sh $path/logic.sh &
+sh $path/logic.sh 1
